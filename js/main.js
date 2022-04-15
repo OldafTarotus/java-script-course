ecb1236cfd6b2ce259a1208c04c1121f9d017a05
OldafTarotus <Mis.Ahmed.Naser@gmail.com>
2022-04-15 21:12:09 +0200

    \ & \n

--- js/main.js
+++ js/main.js
@@ -6,15 +6,15 @@
     Syntax
 */
 // this mean start work after load
-window.onload = function () {
-    document.querySelector("h1").style.color="blue";
-    document.querySelector("h1").style.fontSize="250px";
-};
+// window.onload = function () {
+//     document.querySelector("h1").style.color="blue";
+//     document.querySelector("h1").style.fontSize="250px";
+// }
 
 // window.alert("click and you will lose your file");
 // document.write("<h2>this page to <span>know</span> a little about js code</h2>");
 // console.log("You can see all action from here");
-------------------------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------------------------
 /*
     Console Methods <---------------
     - log
@@ -24,19 +24,19 @@ window.onload = function () {
     Web Api
 
     Styling Console
-    - Directive %c
+    - Directive  %c
 */
 
-console.log("Welcome to Js");
-console.error("You will Lose Your Window");
-console.table(["Ahmed", "Mohamed","Naser"]);
+// console.log("Welcome to Js");
+// console.error("You will Lose Your Window");
+// console.table(["Ahmed", "Mohamed","Naser"]);
 
-console.log("Hello From %cOldaf %cTarotus","color: red; font-size: 30px", "color: green; font-size: 30px");
+// console.log("Hello From %cOldaf %cTarotus","color: red; font-size: 30px", "color: green; font-size: 30px");
 
-var anything = "ahmed";
-console.log("Hello " + anything);
-console.log('hello ${anything}');
-------------------------------------------------------------------------------------------------------------------------------
+// var anything = "ahmed";
+// console.log("Hello " + anything);
+// console.log('hello ${anything}');
+// ------------------------------------------------------------------------------------------------------------------------------
 /*
     Data Types Intro
     - String => Any character
@@ -46,19 +46,19 @@ console.log('hello ${anything}');
     - Boolean
 */
 
-console.log("Oldaf Tarouts");  
-console.log(typeof "Oldaf Tarotus");                                  String
-console.log(typeof 5000);                                             Number
-console.log(typeof 5000.99);                                          Number
-console.log(typeof [10, 15, 20]);                                     Array  - Opject
-console.log(typeof ["Ol", "Ta", "Ah"]);                               Array  - Opject
-console.log(typeof {name: "Oldaf", age: 30, country: "eg"});          Opject
-console.log(typeof true);                                             Boolean
-console.log(typeof false);                                            Boolean
-console.log(typeof undefined);                                        Undefined
-console.log(typeof null);                                             Opject
+// console.log("Oldaf Tarouts");  
+// console.log(typeof "Oldaf Tarotus");                                  String
+// console.log(typeof 5000);                                             Number
+// console.log(typeof 5000.99);                                          Number
+// console.log(typeof [10, 15, 20]);                                     Array  - Opject
+// console.log(typeof ["Ol", "Ta", "Ah"]);                               Array  - Opject
+// console.log(typeof {name: "Oldaf", age: 30, country: "eg"});          Opject
+// console.log(typeof true);                                             Boolean
+// console.log(typeof false);                                            Boolean
+// console.log(typeof undefined);                                        Undefined
+// console.log(typeof null);                                             Opject
 
-------------------------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------------------------
 /*
     Variables intro
     - what is variable ?
@@ -70,31 +70,31 @@ console.log(typeof null);                                             Opject
     - loosely typed vs strongly types
 */
 
-var user ="Ahmed"
-var age ="30"
-or 
+// var user ="Ahmed"
+// var age ="30"
+// or 
 // var user ="ahmed", age = 30;
 
-console.log(user);
-console.log =(age);
+// console.log(user);
+// console.log =(age);
 
 // change any att from document 
-console.log("Hello");
-hello.innerhtml ="option"
-------------------------------------------------------------------------------------------------------------------------------
+// console.log("Hello");
+// hello.innerhtml ="option"
+// ------------------------------------------------------------------------------------------------------------------------------
 /*
     Identifiers
     - Name conventions and rules
     - Reserved words
 */
 
-var UserName ="Ahmed";
-console.log(UserName);
-or 
-console.log(User_Name);
-console.log(User-Name);
-console.log(user_name); etc  must be first character no a number
------------------------------------------------------------------------------------------------------------------------------
+// var UserName ="Ahmed";
+// console.log(UserName);
+// or 
+// console.log(User_Name);
+// console.log(User-Name);
+// console.log(user_name); etc  must be first character no a number
+// -----------------------------------------------------------------------------------------------------------------------------
 
 /*
     var     <<<<<<<<<<
@@ -115,4 +115,20 @@ console.log(user_name); etc  must be first character no a number
     - Variable Scope Drama [Added to Window] ()
     - Block or Function Scope
 */
--------------------------------------------------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------------------------------------
+/*
+    String Syntax + Character Escape Sequences
+    \ Escape + Line Continue
+    \n
+*/
+console.log('Ahmed Naser "Elsayed"')
+console.log("Ahmed Naser 'Elsayed'")
+console.log('Ahmed Naser \'Elsayed\'')
+console.log("Ahmed Naser \"Elsayed\"")
+console.log("Ahmed \\Naser \"Elsayed\"")
+console.log("ahmed \
+Naser \
+Elsayed")
+console.log("Ahmed \nNaser \nElsayed")
+
+
