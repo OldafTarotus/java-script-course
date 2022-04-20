1521575b650f69a410cba2728e6a308fedf986b7
OldafTarotus <Mis.Ahmed.Naser@gmail.com>
2022-04-20 17:35:36 +0200

    Logical Operators

--- js/main.js
+++ js/main.js
@@ -582,5 +582,22 @@ Template Literals (Template Strings)
 // console.log(typeof "Ahmed" === typeof "Mohamed"); //  Both same tring & Same value by typeof
 // -------------------------------------------------------------------------------------------------------------------------
 
+/*
+    Logical Operators
+    - ! Not
+    - && And 
+    - || OR
+*/
+
+// console.log(true);
+// console.log(!true);
+
+// console.log(!(10 == "10"));
+
+// console.log(10 == "10" && 10 > 8 && 10 > 50);
+
+// console.log(10 == "10" || 10 > 8 || 10 > 50);
+// -------------------------------------------------------------------------------------------------------------------------
+
 
 
