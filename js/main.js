b7ad3cbe6a9ecd5083d6faf6aef7ef3395bce830
OldafTarotus <Mis.Ahmed.Naser@gmail.com>
2022-05-12 22:18:25 +0200

    Map (Swap Cases - inverted numbers - ignore boolean value)

--- js/main.js
+++ js/main.js
@@ -1500,3 +1500,36 @@
 // console.log(add);
 //------------------------------------------------------------------------------------------------------------------------
 
+/*
+    Map
+    - Swap Cases
+    - Inverted Numbers
+    - Ignore Boolean Value
+*/
+
+// let swappingCases = "Ahmed";
+// let invertedNumbers = [1, -10, -20, 15, 100, -30];
+// let ignoreNumbers = "Ah123med";
+
+// let sw = swappingCases.split("").map(function (ele) {
+//     // Condition > True : False
+//     return ele === ele.toUpperCase() ? ele.toLowerCase() : ele.toUpperCase()
+// });
+// console.log(sw);
+
+// arrow version   <<<<<<<<<<<<<<<<<<<<<<<< 
+// Condition > True : False
+// let sw = swappingCases.split("").map((a) => a === a.toUpperCase() ? a.toLowerCase() : a.toUpperCase()
+// );
+// console.log(sw);
+
+// let inv = invertedNumbers.map(function (ele) {
+//     return -ele;
+// }); 
+// console.log(inv);
+
+// let ign = ignoreNumbers.split("").map(function (ele) {
+//     return isNaN(parseInt(ele)) ? ele : "";
+// }).join("");
+// console.log(ign);
+//------------------------------------------------------------------------------------------------------------------------
