c92737f1b25f27fbd90a8dab0f8e63536abc8609
OldafTarotus <Mis.Ahmed.Naser@gmail.com>
2022-05-12 20:02:55 +0200

    Function Arrow Challenges

--- js/main.js
+++ js/main.js
@@ -1421,3 +1421,30 @@
 // }
 // grand();
 //------------------------------------------------------------------------------------------------------------------------
+
+/*
+    Function Arrow Challenges
+*/
+// [1] One Statement In Function
+// [2] Convert To Arrow Function
+// [3] Print The Output [Arguments May Change]
+
+// let names = function () {
+//     // parameter ... ?
+//     return "???";
+// }
+// console.log(names("Osama", "Mohamed", "Ali", "Ibrahim"));
+// // String [Osama], [Mohamed], [Ali], [Ibrahim], => Done !
+// /*--------------*/
+
+// // [1] Replace ??? In Return Statment To Get Output
+// // [2] Creat The Same Function With Regular Syntax
+// // [3] Use Array Inside The Arguments To Get The Output
+
+// let myNumber = [20, 50, 10, 60];
+
+// let calc = (One, Two, ...nums) => "???";
+
+// console.log(calc(10, "???", "???"));  // 80
+//------------------------------------------------------------------------------------------------------------------------
+
