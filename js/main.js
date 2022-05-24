8f9683b740932b491a7531df84efc94405c2f47d
OldafTarotus <Mis.Ahmed.Naser@gmail.com>
2022-05-24 23:17:18 +0200

    BOM (Session Storage (setItem - getItem - removeItem - clear - Key))

--- js/main.js
+++ js/main.js
@@ -2651,4 +2651,32 @@
 // });
 //------------------------------------------------------------------------------------------------------------------------
 
+/*
+    BOM [Browser Object Model]
+    session Storage
+    - setItem
+    - getItem
+    - removeItem
+    - clear
+    - key
+
+    info
+    - New Tab = New Session
+    - Duplicate Tab = Copy Session
+    - New Tab With Same Url = New Session
+*/
+
+// window.localStorage.setItem("color", "red");
+// window.sessionStorage.setItem("color", "blue");
+
+// document.querySelector(".name").onblur = function () {
+//     console.log(this.value);
+//     window.localStorage.setItem("input-name", this.value);
+// };
+//------------------------------------------------------------------------------------------------------------------------
+
+/*
+    
+*/
+
 
