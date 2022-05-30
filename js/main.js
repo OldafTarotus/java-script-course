41cd192c6ba429714f95427aea9d1f24b4831e0e
OldafTarotus <Mis.Ahmed.Naser@gmail.com>
2022-05-30 23:40:40 +0200

    Array Methods

--- js/main.js
+++ js/main.js
@@ -3122,7 +3122,40 @@
 // console.log(myWeakMap);
 // -------------------------------------------------------------------------------------------------------------------------
 
+/*
+    Array Methods
+    - Array.From(Iterable, MapFunc, This)
+    --- Variable
+    --- String Numbers
+    --- Set
+    --- Using The Map Function
+    --- Arrow Functino
+    --- Shorten The Method + Use Arguments 
+*/
 
+// console.log(Array.from("Osama"));
+
+// console.log(Array.from("12345", function (n) {
+//     return +n + +n;
+// })
+// );
+
+// console.log(Array.from("12345", (n) => +n + +n));
+
+// let myArray = [1, 1, 1, 2, 3, 4];
+
+// let mySet = new Set(myArray);
+
+// console.log(Array.from(mySet));
+
+// // console.log([...new Set(myArray)]); // Future
+
+// function af() {
+//     return Array.from(arguments);
+// }
+
+// console.log(af("Osama", "Ahmed", "sayed", 1, 2, 3));
+// // -------------------------------------------------------------------------------------------------------------------------
 
 
 
