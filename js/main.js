ec9a9ab6e5d32109266a6b1cb27cd9834df5e277
OldafTarotus <Mis.Ahmed.Naser@gmail.com>
2022-06-03 00:59:43 +0200

    Regular Expression (Email - IP - Phone - URL)

--- js/main.js
+++ js/main.js
@@ -3315,5 +3315,31 @@
 //     D: 4,
 // };
 // console.log({...object1, ...object2, e: 5});
+//-------------------------------------------------------------------------------------------------------------------------
+
+/*
+    Regular Expression
+    - Email
+    - IP
+    - Phone
+    - URL
+*/
+
+// let str1 = '10 20 100 1000 5000';
+// let str2 = 'Os1 Os12 Os123 Os123Os Os12312Os123';
+
+// let invalidEmail = 'ahmed@@@gmail....com';
+// let validEmail = 'a@egytrade.it';
+
+// let ip = '192.168.2.1'; // IPv4
+
+// let url = 'elzero.org';
+// let url = 'elzero.org/';
+// let url = 'http://elzero.org/';
+// let url = 'http://www.elzero.org/';
+// let url = 'https://.elzero.org/';
+// let url = 'https://www.elzero.org/';
+// let url = 'https://www.elzero.org/?facebookid=asdasdasd';
+//-------------------------------------------------------------------------------------------------------------------------
 
 
