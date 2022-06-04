c87b5b7d26b9ea35c75101727436db2d2ee8860b
OldafTarotus <Mis.Ahmed.Naser@gmail.com>
2022-06-04 21:11:09 +0200

    Regular Expression (i => case-insensitive g => global m => Multilines)

--- js/main.js
+++ js/main.js
@@ -3342,4 +3342,33 @@
 // let url = 'https://www.elzero.org/?facebookid=asdasdasd';
 //-------------------------------------------------------------------------------------------------------------------------
 
+/*
+    Regular Expression
+
+    Syntax
+    /pattern/modifier(s);
+    new RegExp("pattern", "modifier(s)")
+
+    Modifiers => Flags
+    i => case-insensitive
+    g => global
+    m => Multilines
+
+    Search Methods
+    - match(Pattern)
+
+    Match
+    -- Matches A String Against a Regular Expression Pattern
+    -- Returns An Array With The Matches
+    -- Returns null If No Match Is Found.
+*/
+
+// let myString = "Hello Oldaf Web School I Love oldaf";
+
+// let regex = /oldaf/ig;
+
+// console.log(myString.match(regex));
+//-------------------------------------------------------------------------------------------------------------------------
+
+
 
