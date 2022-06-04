956ce15ccd813fc7e964419d3b05a15d1ac03949
OldafTarotus <Mis.Ahmed.Naser@gmail.com>
2022-06-05 01:58:33 +0200

    Regular Expression Character Classes (\b at start or the end & \B)

--- js/main.js
+++ js/main.js
@@ -3478,3 +3478,29 @@
 // console.log(email.match(nonCharacter));
 // console.log(email.match(valid));
 //-------------------------------------------------------------------------------------------------------------------------
+
+/*
+    Regular Expression
+    Character Classes
+    \b => matches at the beginning or end of a word
+    \B => matches Not at the beginning/end of a word
+
+    Test Method
+    pattern.test(input)
+*/
+
+// let names = "Sayed 1Spam 2Spam 3Spam Spam4 Spam5 Osama Ahmed Aspamo"
+// let re = /spam/ig;
+// let rebstart = /\bspam/ig;
+// let rebend = /spam\b/ig;
+// let reball = /(\bspam|spam\b)/ig;
+
+// console.log(names.match(re));
+// console.log(names.match(rebstart));
+// console.log(names.match(rebend));
+// console.log(names.match(reball));
+
+// console.log(/(\bspam|spam\b)/ig.test("ahmed"));
+// console.log(/(\bspam|spam\b)/ig.test("1Spam"));
+// console.log(/(\bspam|spam\b)/ig.test("Spam1"));
+//-------------------------------------------------------------------------------------------------------------------------
