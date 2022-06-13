b604afbcc6024a126e508a08e5f98b9ba63a697b
OldafTarotus <Mis.Ahmed.Naser@gmail.com>
2022-06-13 22:47:42 +0200

    Encapsulation - Private Property

--- js/main.js
+++ js/main.js
@@ -3815,7 +3815,33 @@
 // console.log(One.sayHello());
 //-------------------------------------------------------------------------------------------------------------------------
 
+/*
+    Encapsulation
+    - Class Fields Are Public By Default
+    - Guards The Data Against Illegal Access.
+    - Helps To Achieve The Target Without Revealing Its Complex Details.
+    - Will Reduce Human Errors.
+    - Make The App More Flexible And Manageable.
+    - Simplifies The App.
+*/
 
+// class User {
+//     // Private Property
+//     #e;
+//     constructor(id, username, eSalary) {
+//         this.i = id;
+//         this.u = username;
+//         this.#e = eSalary;
+//     }
+//     getSalary() {
+//         return parseInt(this.#e);
+//     }
+// }
+
+// let One = new User(100, "Ahmed", "500 gneh");
+
+// console.log(One.u);
+// console.log(One.getSalary() * .3);
 
 
 
