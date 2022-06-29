3c9ea67f0a11dae7f678d6bd9cc1d0307e4ab587
OldafTarotus <Mis.Ahmed.Naser@gmail.com>
2022-06-29 22:52:05 +0200

    Date And Time (Get)

--- js/main.js
+++ js/main.js
@@ -4022,6 +4022,39 @@
 // console.log(`Years ${years}`);
 //-------------------------------------------------------------------------------------------------------------------------
 
+/*
+    Date And Time
+    - getTime() => Number Of Milliseconds
+    - getDate() => Day Of The Month
+    - getFullYear()
+    - getMonth() => Zero Based
+    - getDay() => Day Of The Week
+    - getHours()
+    - getMinutes()
+    - getSeconds()
+*/
+
+// let dateNow = new Date();
+// let birthDay = new Date("feb 15 91");
+// let dateDiff = dateNow - birthDay;
+
+// console.log(dateDiff);
+// console.log(dateDiff / 1000 / 60 / 60 / 24 / 365);
+
+// console.log(dateNow);
+// console.log(dateNow.getTime());
+// console.log(dateNow.getDate());
+// console.log(dateNow.getFullYear);
+// console.log(dateNow.getMonth());
+// console.log(dateNow.getDay());
+// console.log(dateNow.getHours());
+// console.log(dateNow.getMinutes());
+// console.log(dateNow.getSeconds());
+//-------------------------------------------------------------------------------------------------------------------------
+
+
+
+
 
 
 
