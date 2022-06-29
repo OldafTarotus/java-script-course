7cb185bc0b23bd30b9016bed22f3da476a8af4f7
OldafTarotus <Mis.Ahmed.Naser@gmail.com>
2022-06-29 20:26:25 +0200

    Object Meta Data And Descriptor (Define Multiple Properties - Check Descriptors)

--- js/main.js
+++ js/main.js
@@ -3955,3 +3955,36 @@
 // console.log(myObject);
 //-------------------------------------------------------------------------------------------------------------------------
 
+/*
+    Object Meta Data And Descriptor
+    - Define Multiple Properties
+    - Check Descriptors
+*/
+
+// const myObject = {
+//     a: 1,
+//     n: 2,
+// };
+
+// Object.defineProperties(myObject, {
+//     c: {
+//         configurable: true,
+//         value:3,
+//     },
+//     d: {
+//         configurable: true,
+//         value: 4,
+//     },
+//     e: {
+//         configurable: true,
+//         value: 5,
+//     },
+// });
+// console.log(myObject);
+// console.log(Object.getOwnPropertyDescriptor(myObject, "d"));
+// console.log(Object.getOwnPropertyDescriptors(myObject));
+//-------------------------------------------------------------------------------------------------------------------------
+
+
+
+
